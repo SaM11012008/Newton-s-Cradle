ca21344b87217c60af326242c58d560bfc41e5ef
SaM11012008 <74596262+SaM11012008@users.noreply.github.com>
2021-01-17 15:09:05 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,27 @@
+class Bob{
+    constructor(x,y,radius){
+        var opt = {
+            isStatic:false,
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2,
+        }
+
+        this.body = Matter.Bodies.circle(x,y,radius,opt);
+        this.radius = radius;
+
+        World.add(world,this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        var radius = this.body.radius;
+        push()
+        translate(pos.x,pos.y);
+        ellipseMode(CENTER);
+        fill("red");
+        circle(0,0,this.radius);
+        pop()
+    }
+
+}
